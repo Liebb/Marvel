7d220c644deb766171864d94941cea1a390d8e91
Liebb <liebermorazan@gmail.com>
2022-07-27 23:06:14 -0600

    :sparkles: Final project
    The final project is ended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
+  darkMode: 'class',
     theme: {
         colors: {
           ...colors,
-            primary:"#dc2626",
+            primary:"#581c87",
             secondary:"#f5f5f5",
             tertiary:"#27272a",
           },
@@ -14,21 +15,9 @@ module.exports = {
       extend: {
         backgroundImage: {
             'moonKnight': "url('../img/MoonKnight.png')",
+            'portada': "url('../img/portada.jpg')",
             'sanFrancisco': "url('../img/sanFrancisco.jpg')",
             'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
-            'yosemite': "url('../img/yosemite.jpg')",
-            'LA': "url('../img/LA.jpg')",
-            'seattle': "url('../img/seattle.jpg')",
-            'new_york': "url('../img/new_york.jpg')",
-            'norway': "url('../img/norway.jpg')",
-            'sydney': "url('../img/sydney.jpg')",
-            'miami': "url('../img/miami.jpg')",
-            'switzerland': "url('../img/switzerland.jpg')",
-            'bali': "url('../img/bali.jpg')",
-            'norway': "url('../img/norway.jpg')",
-            'chicago': "url('../img/chicago.jpg')",
-            'europe': "url('../img/europe.jpg')",
-            'iceland': "url('../img/iceland.jpg')",
         }
       },
     },
